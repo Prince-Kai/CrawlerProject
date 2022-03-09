76a91a90236c83b16f129a5a1881d715c5cc5281
PrinceKai <94831453+Prince-Kai@users.noreply.github.com>
2022-03-09 17:52:48 +0800

    Update main.py
    （1）引入logging对象，增加日志样式配置。

--- desktop_wallpaper/main.py
+++ desktop_wallpaper/main.py
@@ -5,10 +5,14 @@
 # @Author  : Kai Prince
 # @File    : main.py
 # +-------------------------------------------------------------------
-
+import logging
+import os
+from datetime import datetime
 
 from data_processing import BasicInformationDownload
 from downloader import WallpaperDownloader
+
+LEVELS = [logging.DEBUG, logging.INFO, logging.WARNING, logging.ERROR, logging.CRITICAL]
 
 
 def main():
@@ -16,9 +20,19 @@ def main():
     主控制函数，用于调度data_processing、downloader文件的Python程序
     :return:
     """
+    # 生成日志文件夹
+    if not os.path.exists("logs"):
+        os.mkdir("logs")
+
+    # 设置主进程的日志等级和输出日志格式
+    logging.basicConfig(level=LEVELS[0],
+                        filename=f"""logs/log{datetime.now().strftime("%Y-%m-%d")}-主进程.txt""",
+                        filemode="a",
+                        format='【%(asctime)s】-【%(filename)s】【line:%(lineno)d】【%(levelname)s】: %(''message)s')
+
     # 实例化。调用基础信息生成方法，调用过后避免重复调用
-    # basic_information = BasicInformationDownload()
-    # basic_information.main()
+    basic_information = BasicInformationDownload()
+    basic_information.main()
 
     # 实例化。调用壁纸下载方法
     wallpaper_downloader = WallpaperDownloader()
